b4d2ebcc2fd277271b3fb136da3ebda41eb47367
Santhiya <169180434+Santhiya-sm-15@users.noreply.github.com>
2025-09-25 20:16:53 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public ArrayList<String> generateBinary(int n) {
+        ArrayList<String> res=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+            res.add(Integer.toBinaryString(i));
+        return res;
+    }
+}
